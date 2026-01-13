def361050f5710215ea7b65fd2220803f14c5fc2
Alba Panato Alegre <alba.panato.alegre.apa@gmail.com>
2026-01-13 13:43:53 +0100

    Eliminación de Tailwind y migración a Bootstrap en el frontend

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
